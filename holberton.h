e39aec5afffa0ab2c4ad7bc0793109308cbda2b6
juanesart <juanesartq@gmail.com>
2020-08-16 16:34:47 +0000

    Add header file

---
+++ holberton.h
@@ -0,0 +1,28 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+list_t *add_node(list_t ***head, const char *str);
+char *split(char *name, list_t **head);
+void free_list(list_t *head);
+list_t *add_node2(list_t **head, const char *str);
+
+
+#endif
