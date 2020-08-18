a21c59f0ec84056d554bb590fdaa89942599ba08
juanesart <juanesartq@gmail.com>
2020-08-18 19:59:40 +0000

    Update token function implementation

--- main_shell.c
+++ main_shell.c
@@ -18,19 +18,14 @@ void _free(char **string)
 int main(__attribute__((unused))int ac, __attribute__((unused))char **av, char **env)
 {
 	
-	char *string = NULL; /*variabe to get command keyboard*/
 	size_t strsize = 0; /*variabe to getline int size type*/
 	pid_t child_pid; /*variable to fork*/
 	int status, sw, aw = 0;  /*variable to wait*/
-	char *aux;
+	char *aux, *temp, **savingtok = NULL, *string = NULL, *path = "PATH=";
 	struct stat st;
 	unsigned int i = 0, j = 0;
-	char *path = "PATH=";
-	list_t *h = NULL;
-	list_t *auxh = NULL;
-	list_t *hreset = NULL;
-	char *temp;
-        char **savingtok = NULL;
+	list_t *h = NULL, *auxh = NULL, *hreset = NULL;
+
 	while (env[i] != NULL)
 	{
 		if (*env[i] == path[j])
@@ -43,7 +38,7 @@ int main(__attribute__((unused))int ac, __attribute__((unused))char **av, char *
 	printf("$ ");
 	while (getline(&string, &strsize, stdin) != EOF)
 	{
-                aux = _strtok(string, "\n");
+		aux = _strtok(string, "\n");
 		hreset = h;
 		while (h)
 		{
@@ -54,40 +49,43 @@ int main(__attribute__((unused))int ac, __attribute__((unused))char **av, char *
 		while (h)
 		{
 			temp = auxh->str;
-                        if (*aux == '/')
-                        {
-                                savingtok = tokens(aux, temp);
-                                aw = 1;
-                        }
-                        else
-                                savingtok = tokens(aux, temp);
-			
-			if (stat(savingtok[0], &st) == 0 && aux != NULL)
+
+			if (aux != NULL)
 			{
-				child_pid = fork();
-				if (child_pid == -1)
+				if (*aux == '/')
 				{
-					perror("child: error");
-					exit (EXIT_FAILURE);
-				}
-				if (child_pid == 0)
-				{
-					if (execve(savingtok[0], savingtok, NULL) == -1)
-					{
-						perror("execve: error");
-						exit(EXIT_FAILURE);
-					}
+					savingtok = tokens(aux, temp);
+					aw = 1;
 				}
 				else
+					savingtok = tokens(aux, temp);
+				if (stat(savingtok[0], &st) == 0)
 				{
-					wait(&status);
+					child_pid = fork();
+					if (child_pid == -1)
+					{
+						perror("child: error");
+						exit (EXIT_FAILURE);
+					}
+					if (child_pid == 0)
+					{
+						if (execve(savingtok[0], savingtok, NULL) == -1)
+						{
+							perror("execve: error");
+							exit(EXIT_FAILURE);
+						}
+					}
+					else
+					{
+						wait(&status);
+					}
+					sw = 1;
 				}
-				sw = 1;
-			}
-			else if (h->next == NULL)
-			{
-				if (sw != 1 && aux != NULL)
-					printf(" %s: command not found [%d]\n", aux, sw);
+				else if (h->next == NULL)
+				{
+					if (sw != 1 && aux != NULL)
+						printf(" %s: command not found [%d]\n", aux, sw);
+				}
 			}
 			if (sw == 1 && aw == 1)
 				break;
@@ -99,11 +97,11 @@ int main(__attribute__((unused))int ac, __attribute__((unused))char **av, char *
 		h = hreset;
 		printf("$ ");
 	}
-        /* _free(auxt);
-        _free(test); */
-        /* free(temp); */
-        /* free_list(hreset); */
-        free_list(h);
+	/* _free(auxt);
+	_free(test); */
+	/* free(temp); */
+	/* free_list(hreset); */
+	free_list(h);
 	free(string);
 	return (0);
 }
